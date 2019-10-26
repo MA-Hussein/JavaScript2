296a7d4256cea07eed08c9c2335ae3dc7c1000a4
MA-Hussein <54192398+MA-Hussein@users.noreply.github.com>
2019-10-26 23:38:44 +0200

    Update 1-booklist.js

--- Week1/homework/JS-Eexercises/1-booklist.js
+++ Week1/homework/JS-Eexercises/1-booklist.js
@@ -18,7 +18,7 @@ const myLibrary = [
   }
 	
 ];
-
+// books covers as per their order in the above array.
 const books_covers = [
  
 	"https://images-na.ssl-images-amazon.com/images/I/51y4bQIDZEL._SX322_BO1,204,203,200_.jpg",
@@ -26,19 +26,16 @@ const books_covers = [
 	"https://s.s-bol.com/imgbase0/imagebase3/large/FC/9/9/8/0/1001004006240899.jpg"
 	
 ];
-
+const myFavoriteBook = document.createElement("ul");
 myLibrary.forEach(element => {
-
-  const myFavoriteBook = document.createElement("ul");
 
   const book = document.createElement("li");
 
   const bookInfo = document.createElement("p");
-  bookInfo.textContent = element.title;
-  bookInfo.textContent += ": " + element.author;
+  bookInfo.textContent = element.title + ": " + element.author;
 
   const bookCover = document.createElement("img");
-  bookCover.src = books_covers[myLibrary.indexOf(element)];
+  bookCover.src = books_covers[myLibrary.indexOf(element)];// books covers as per their order in the above array.
   book.alt = element.title;
   bookCover.height = 300;
   bookCover.width = 192;
@@ -46,9 +43,6 @@ myLibrary.forEach(element => {
   book.appendChild(bookInfo);
   book.appendChild(bookCover);
   myFavoriteBook.appendChild(book);
-
-  element.alreadyRead
-    ? (bookInfo.style.color = "green")
-    : (bookInfo.style.color = "red");
+  bookInfo.style.color = element.alreadyRead ? "green" :"red";
   document.body.appendChild(myFavoriteBook );
 });
