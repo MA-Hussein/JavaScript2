5bd3a5855b0cce230d6c0cf179e404fbf289e59c
MA-Hussein <54192398+MA-Hussein@users.noreply.github.com>
2019-10-26 22:31:40 +0200

    Update 1-oddOnesOut.js

--- Week2/homework/JS-Exercises/1-oddOnesOut.js
+++ Week2/homework/JS-Exercises/1-oddOnesOut.js
@@ -6,8 +6,8 @@ const myNumbers = [1, 2, 3, 4];
   function doubleEvenNumbers(myNumbers) {
 
     const filterdNumbers = myNumbers.filter(number => number % 2 === 0); // Get the even numbers with filter method.
-	const doubledEven=filterdNumbers.map(number => number * 2); //apply multiplication on every even number in the array using map method.
-    return doubledEven;
+
+    return filterdNumbers.map(number => number * 2);//apply multiplication on every even number in the array using map method.
   };
   console.log(doubleEvenNumbers(myNumbers));
 
