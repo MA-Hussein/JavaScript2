0eefc1b4ca31e1ceb441def63cecb9e0802baa32
MA-Hussein <54192398+MA-Hussein@users.noreply.github.com>
2019-10-27 00:05:20 +0200

    Update showCurrentTime.js

--- Week1/homework/JS-Eexercises/showCurrentTime.js
+++ Week1/homework/JS-Eexercises/showCurrentTime.js
@@ -6,7 +6,11 @@ function displayTime() {
   const m = today.getMinutes();
   const s = today.getSeconds();
 
-  document.getElementById("displayTime").textContent = h + ":" + m + ":" + s;
+   document.getElementById("displayTime").textContent = `${format(h)}:${format(m)}:${format(s)}`;
+}
+
+function format(n) {
+  return n > 9 ? n : `0${n}`;
 }
 
 window.onload = setInterval(displayTime, 1000);
